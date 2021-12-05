1b195c6df3674ba3681fe75d657f757e40074ae3
samet-nalbant <s.nalbant@outlook.com>
2021-12-05 20:51:50 +0300

    Firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,58 @@ import 'package:firebase_core/firebase_core.dart';
 
 const color = const Color(0xff4a3564);
 const textBoxColor = const Color(0xffe269fc);
-Future<void> main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(App());
+}
+
+/// We are using a StatefulWidget such that we only create the [Future] once,
+/// no matter how many times our widget rebuild.
+/// If we used a [StatelessWidget], in the event where [App] is rebuilt, that
+/// would re-initialize FlutterFire and make our application re-enter loading state,
+/// which is undesired.
+class App extends StatefulWidget {
+  // Create the initialization Future outside of `build`:
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp(
+      options: FirebaseOptions(
+    apiKey: "AIzaSyA92-Rfwm3txHTNbUKVxIKam_ahEp-4RFg",
+    authDomain: "sportal-881de.firebaseapp.com",
+    projectId: "sportal-881de",
+    storageBucket: "sportal-881de.appspot.com",
+    messagingSenderId: "844614521147",
+    appId: "1:844614521147:web:a0ae93dad96f401770e71e",
+    measurementId: "G-6EZ9L4105R",
+  ));
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Text(snapshot.error.toString(),
+              textDirection: TextDirection.ltr);
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return CircularProgressIndicator();
+      },
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
